2fc172d39e7aa91be9cb958a1a74bee97d4ca1b4
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2022-03-16 17:39:56 +0200

    my stuffs

--- dist/src/entity/tictactoe.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class Tictactoe {
-    id: number;
-    current_status: string;
-}
--- dist/src/titactoe.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { TitactoeService } from './titactoe.service';
-export declare class TitactoeController {
-    private readonly titactoeService;
-    constructor(titactoeService: TitactoeService);
-    getBoard(playing: any): string;
-}
